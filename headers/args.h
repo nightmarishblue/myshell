ff229d57acd3d13c1393eded5b6a95593d988a8e
blue <midnight@nightmarish.blue>
2024-02-27 15:17:01 +0000

    Words enclosed by quotes are now grouped into a single argument.

--- headers/args.h
+++ headers/args.h
@@ -2,7 +2,8 @@
 
 #define MAX_CMD_LEN 256 // the maximum length of an input command
 #define MAX_ARGS 64 // the maximum number of parsed arguments.
-#define ARG_WHITESPACE " \n\t" // a rudimentary macro that contains whitespace.
+#define ARG_WHITESPACE " \n\t\r" // a rudimentary macro that contains whitespace.
+#define ARG_QUOTES "\"'`" // the chars that are considered quotes and will group words together
 
 // take a cmd buffer and split it into tokens, placing each into the argarr
 // returns the number of args found, which cannot be more than MAX_ARGS
--- libraries/args.c
+++ libraries/args.c
@@ -18,14 +18,59 @@ int parsebuiltin(const char cmd[])
     return -1;
 }
 
+// wrapper around strtok that takes quotes into account.
+char* argindexpointer = NULL;
+char* tokarg(char* __restrict__ string, const char* __restrict__ delim, const char* __restrict__ quote)
+{
+    if (string == NULL)
+        string = argindexpointer; // use our stored index like strtok
+    if (string == NULL)
+        return NULL; // basic error handling
+
+    // if the string starts with delim, we need to ignore it and move up
+    string += strspn(string, delim);
+    char first = string[0];
+
+    if (first == '\0') // handle the string being empty
+    {
+        argindexpointer = NULL;
+        return NULL;
+    }
+
+    // if first is a quote; we need to group it up with its pair
+    if (strchr(quote, first) != NULL) // check if this is a quote
+    {
+        char* endquote = strchr(string + 1, first); // find the next quote in the string
+        if (endquote != NULL) // if it exists, we have a match
+        {
+            *endquote = '\0'; // terminate the endquote
+            argindexpointer = endquote + 1; // set our index pointer to the next char
+            return string + 1; // return the pointer; this will make a string that contains the quoted text
+        }
+    }
+    
+    // case 3: normal char, just split to the next space
+    char* whitespace = strpbrk(string, delim);
+    if (whitespace == NULL || *whitespace == '\0') // if this is a null, then we are at the end of the string
+    {
+        argindexpointer = NULL;
+    } else
+    {
+        *whitespace = '\0';
+        argindexpointer = whitespace + 1;
+    }
+    return string;
+}
+
+
 int splitargs(char cmdstr[MAX_CMD_LEN], char* argarr[MAX_ARGS])
 {
     int index = 0; // index to return as length
-    char* arg = strtok(cmdstr, ARG_WHITESPACE); // store the current arg into a pointer
+    char* arg = tokarg(cmdstr, ARG_WHITESPACE, ARG_QUOTES); // store the current arg into a pointer
     while (arg != NULL && index < MAX_ARGS)
     {
         argarr[index++] = arg;
-        arg = strtok(NULL, ARG_WHITESPACE);
+        arg = tokarg(NULL, ARG_WHITESPACE, ARG_QUOTES);
     }
     return index;
 }

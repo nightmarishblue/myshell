6871f0b661599c662240ec216e04347d491180ef
blue <midnight@nightmarish.blue>
2024-03-01 20:52:50 +0000

    Added support for batch scripting.

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 int main(int argc, char* argv[argc])
 {
@@ -15,22 +14,41 @@ int main(int argc, char* argv[argc])
     char* cmdargs[MAX_ARGS];
     int cmdargc;
     
+    FILE* input; // the iostream to pull our commands from
+    int interactive = 1;
+    if (argc > 1) // we have a file to run
+    {
+        interactive = 0;
+        char* filename = argv[1];
+        input = fopen(filename, "r");
+        if (input == NULL)
+        {
+            fprintf(stderr, "msh: could not open file '%s': ", filename);
+            perror("");
+            return 1;
+        }
+    } else
+    {
+        input = stdin;
+    }
+
     while (1)
     {
         // 1. print the prompt
-        printf("8> ");
+        if (interactive) printf("8> ");
 
         // 2. wait for the user to type something, and split it into a list of args
-        char* s = fgets(cmdstr, MAX_CMD_LEN, stdin); // grab a line from stdin
+        char* s = fgets(cmdstr, MAX_CMD_LEN, input); // grab a line from stdin
         strncpy(argbuff, cmdstr, MAX_CMD_LEN); // cmdstr holds the unbroken string
         // split that line into args - argbuff holds the strings in cmdargs
         cmdargc = splitargs(argbuff, cmdargs);
 
         // 3. make sure the user hasn't closed the shell or put in an empty string
-        if (feof(stdin)) exit(0);
+        if (feof(input)) exit(0);
         if (cmdargc == 0) continue; // stop if we somehow have no args
 
         // 4. check if user is redirecting i/o and accomodate
+        // TODO: instead of redirecting the entire program's i/o, return something that we can use to set the subshell's output
         int redirectedio = parseioredirects(cmdargc, cmdargs);
         if (redirectedio) // remove all the redirections from the cmdstring
         {
@@ -40,6 +58,7 @@ int main(int argc, char* argv[argc])
 
         // ?. execute the command
         int builtin = parsebuiltin(cmdargs[0]); // if this cmd is a builtin, find which one it is
+        // TODO make the execution of these commands fork and use exec
         if (builtin == -1)
         {
             system(cmdstr); // execute the system command with the entire string.

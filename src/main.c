d8636f97ceed3e1d5c4fe0932188a0941a96ca63
blue <midnight@nightmarish.blue>
2024-05-06 17:00:58 +0100

    Removed the boilerplate that bloated every file

--- src/main.c
+++ src/main.c
@@ -1,19 +1,3 @@
-/*
-AUTHOR: Niall Bastible Díaz | 22314443 | niall.bastiblediaz2@mail.dcu.ie
-ACKNOWLEDGEMENTS
-    I understand that the University regards breaches of academic integrity and plagiarism as grave and serious.
-
-    I have read and understood the DCU Academic Integrity and Plagiarism Policy.
-    I accept the penalties that may be imposed should I engage in practice or practices that breach this policy.
-
-    I have identified and included the source of all facts, ideas, opinions and viewpoints of others in the assignment references.
-    Direct quotations, paraphrasing, discussion of ideas from books, journal articles, internet sources, module text, or any other
-    source whatsoever are acknowledged and the sources cited are identified in the assignment references.
-
-    I declare that this material, which I now submit for assessment, is entirely my own work and has not been taken from the work of
-    others save and to the extent that such work has been cited and acknowledged within the text of my work.
-*/
-
 #include "main.h"
 
 #include "./headers/args.h"
